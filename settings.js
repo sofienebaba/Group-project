2b1e5bfa7f634cdc4cb519b481ce99d4de3bc3cc
dylancordoni <dylancordoni@gmail.com>
2024-12-01 22:38:20 -0800

    add modal popups for account info changes, fixed styling

--- settings.js
+++ settings.js
@@ -3,6 +3,9 @@ let settingsDiv = document.getElementById("account-info")
 settingsLink.style.color = "#ffcc00"
 settingsDiv.style.display = "block"
 
+
+
+
 function changeSettingsDiv(id) {
     settingsLink.style.color = "#ffffff"
     settingsDiv.style.display = "none"
@@ -15,3 +18,15 @@ function changeSettingsDiv(id) {
     settingsDiv.style.display = "block"
 }
 
+function openEditInfo(id) {
+    editPanel = document.getElementById(id)
+    editPanel.style.display = "block"
+}
+
+function saveChanges() {
+    editPanel.style.display = "none"
+}
+
+function closeModal() {
+    editPanel.style.display = "none"
+}
