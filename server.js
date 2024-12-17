50d944bdc72c8fcb53f18e4f30c5b0393908eba0
dylancordoni <dylancordoni@gmail.com>
2024-12-16 17:30:16 -0800

    Revert "maybe fixed search"
    This reverts commit 5eeb11709d5e0e5ddaaf4e3110f644bb816fdbf6.

--- server.js
+++ server.js
@@ -15,9 +15,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get('/products', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'products.html'));
-});
 
 // Fetch products (for your frontend)
 app.get('/api/products', (req, res) => {
