14963d069fcf8166598837fda7589ee456319332
dylancordoni <dylancordoni@gmail.com>
2024-12-16 21:10:32 -0800

    starting on cart

--- database.js
+++ database.js
@@ -54,16 +54,34 @@ db.serialize(() => {
     CREATE TABLE IF NOT EXISTS cart (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       user_id INTEGER NOT NULL,
-      product_id INTEGER NOT NULL,
-      quantity INTEGER NOT NULL DEFAULT 1,
-      FOREIGN KEY (user_id) REFERENCES users (id),
-      FOREIGN KEY (product_id) REFERENCES products (id)
+      purchased BOOLEAN DEFAULT FALSE,
+      FOREIGN KEY (user_id) REFERENCES users (id)
     )
   `, (err) => {
     if (err) {
       console.error("Error creating cart table:", err.message);
     } else {
       console.log("Cart table created successfully.");
+    }
+  });
+});
+
+// Create a table for cart item
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS cart_items (
+      cart_id INTEGER NOT NULL,
+      product_id INTEGER NOT NULL,
+      quantity INTEGER NOT NULL DEFAULT 1,
+      PRIMARY KEY (cart_id, product_id),
+      FOREIGN KEY (cart_id) REFERENCES cart (id),
+      FOREIGN KEY (product_id) REFERENCES products (id)
+    )
+  `, (err) => {
+    if (err) {
+      console.error("Error creating cart table:", err.message);
+    } else {
+      console.log("Cart Item table created successfully.");
     }
   });
 });
